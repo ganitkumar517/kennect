50226a5bdc76c2085d26b767d32131d913e9e161
ganit <ganit.kumar@codemonk.in>
2024-01-13 13:27:12 +0530

    fix: env file

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+require("dotenv").config();
+console.log(process.env.DB_URL,"rhisis")
 
 const connectDB = async () => {
   try {
-    const mongoURL = "mongodb://localhost:27017/kennect";
-    await mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true });
+    const mongoURL = process.env.DB_URL;
+    await mongoose.connect(mongoURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
+    console.error("Error connecting to MongoDB:", error);
   }
 };
 
