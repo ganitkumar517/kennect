1bb69b59e1a17058e4f681d5d670e70f05afdd2b
ganit <ganit.kumar@codemonk.in>
2024-01-13 14:39:52 +0530

    new connection

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const mongoURL = "mongodb+srv://jeetmehra517:Ganit%40517@cluster0.kw9ft7w.mongodb.net/?retryWrites=true&w=majority";
+    const mongoURL = "mongodb+srv://naman-123:Ynag123@cluster0.ewv2nsm.mongodb.net";
     await mongoose.connect(mongoURL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
